86154d3c9f63085607dd7e783fea1c8a66f8b7ee
Iván Fuertes <ivancamilofuerte>
2020-06-12 11:45:29 -0500

    Complete with a `connectivity` error if `client` fails with an error

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,6 +19,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        client.get(from: url) { _ in }
+        client.get(from: url) { _ in
+            completion(.failure(Error.connectivity))
+        }
     }
 }
