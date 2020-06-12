1618e9ecfaf0ada0f7927f4b50e15cb0728ea270
Iván Fuertes <ivancamilofuerte>
2020-06-12 14:23:13 -0500

    Guarantee that no results are deliver after 'RemoteFeedLoader' has been deallocated, even if the `client` outlives the loader

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,7 +19,11 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        client.get(from: url) { result in
+        client.get(from: url) {[weak self] result in
+            guard self != nil else {
+                return
+            }
+            
             switch result {
             case let .success((data, response)):
                 let result = RemoteFeedMapper.map(data: data, response: response)
