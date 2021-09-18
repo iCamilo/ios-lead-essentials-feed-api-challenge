29862cfc41fea7ddd660cb661d85325d66251958
YourDaddy2000 <47114561+YourDaddy2000@users.noreply.github.com>
2021-09-18 20:37:02 +0300

    pass test_load_deliversInvalidDataErrorOnNon200HTTPResponse

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,8 +21,10 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case .success:
-				break
+			case let .success((_, response)):
+				if response.statusCode == 200 {}
+
+				completion(.failure(Error.invalidData))
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
--- Tests/LoadFeedFromRemoteUseCaseTests.swift
+++ Tests/LoadFeedFromRemoteUseCaseTests.swift
@@ -43,19 +43,19 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
 		})
 	}
 
-//	func test_load_deliversInvalidDataErrorOnNon200HTTPResponse() {
-//		let (sut, client) = makeSUT()
-//
-//		let samples = [199, 201, 300, 400, 500]
-//
-//		samples.enumerated().forEach { index, code in
-//			expect(sut, toCompleteWith: .failure(.invalidData), when: {
-//				let json = makeItemsJSON([])
-//				client.complete(withStatusCode: code, data: json, at: index)
-//			})
-//		}
-//	}
-//
+	func test_load_deliversInvalidDataErrorOnNon200HTTPResponse() {
+		let (sut, client) = makeSUT()
+
+		let samples = [199, 201, 300, 400, 500]
+
+		samples.enumerated().forEach { index, code in
+			expect(sut, toCompleteWith: .failure(.invalidData), when: {
+				let json = makeItemsJSON([])
+				client.complete(withStatusCode: code, data: json, at: index)
+			})
+		}
+	}
+
 //	func test_load_deliversInvalidDataErrorOn200HTTPResponseWithInvalidJSON() {
 //		let (sut, client) = makeSUT()
 //
