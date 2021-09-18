e1069facaef555376b8f15da74014ca90c8779f2
YourDaddy2000 <47114561+YourDaddy2000@users.noreply.github.com>
2021-09-18 21:15:36 +0300

    pass test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,7 +19,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
+			guard self != nil else { return }
 			switch result {
 			case let .success((data, response)):
 				completion(FeedMapper.map(data: data, response: response))
--- Tests/LoadFeedFromRemoteUseCaseTests.swift
+++ Tests/LoadFeedFromRemoteUseCaseTests.swift
@@ -113,20 +113,19 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
 		})
 	}
 
-//
-//	func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-//		let url = URL(string: "http://any-url.com")!
-//		let client = HTTPClientSpy()
-//		var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
-//
-//		var capturedResults = [RemoteFeedLoader.Result]()
-//		sut?.load { capturedResults.append($0) }
-//
-//		sut = nil
-//		client.complete(withStatusCode: 200, data: makeItemsJSON([]))
-//
-//		XCTAssertTrue(capturedResults.isEmpty)
-//	}
+	func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
+		let url = URL(string: "http://any-url.com")!
+		let client = HTTPClientSpy()
+		var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
+
+		var capturedResults = [RemoteFeedLoader.Result]()
+		sut?.load { capturedResults.append($0) }
+
+		sut = nil
+		client.complete(withStatusCode: 200, data: makeItemsJSON([]))
+
+		XCTAssertTrue(capturedResults.isEmpty)
+	}
 
 	// MARK: - Helpers
 
